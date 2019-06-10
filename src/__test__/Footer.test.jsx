8abc7513008a3d7ae2ee46b74700386a1c3be9ee
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2019-06-10 14:12:16 +0200

    Add navigation breadcrumbs (#49)

--- src/__test__/Footer.test.jsx
+++ src/__test__/Footer.test.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Footer from '../js/components/Footer';
 
-jest.mock('../js/modules/radarService', () => ({
-  model: {
-    title: 'Test',
-    version: '0.0.1-local',
-    formattedDate: '5 June 2019',
-  },
-}));
+jest.mock('../js/modules/radarService');
 
 describe('<Footer />', () => {
   test('It renders the footer', () => {
--- src/__test__/Radar.test.jsx
+++ src/__test__/Radar.test.jsx
@@ -1,41 +1,9 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { shallow, mount } from 'enzyme';
 import Radar from '../js/components/Radar';
 
-jest.mock('../js/modules/radarService', () => ({
-  listBlips: () => [
-    {
-      label: 'Java',
-      quadrant: 2,
-      ring: 0,
-      moved: false,
-      link: '/entries/java.html',
-      active: true,
-    },
-    {
-      label: 'PHP',
-      quadrant: 2,
-      ring: 3,
-      moved: false,
-      link: '/entries/php.html',
-      active: true,
-    },
-  ],
-  model: {
-    quadrants: [
-      { name: 'Test 1', dirname: 'test1' },
-      { name: 'Test 2', dirname: 'test2' },
-      { name: 'Test 3', dirname: 'test3' },
-      { name: 'Test 4', dirname: 'test4' },
-    ],
-    rings: [
-      'ADOPT',
-      'TRIAL',
-      'ASSESS',
-      'HOLD',
-    ],
-  },
-}));
+jest.mock('../js/modules/radarService');
 
 describe('<Radar />', () => {
   beforeAll(() => {
@@ -55,13 +23,23 @@ describe('<Radar />', () => {
   });
 
   test('It renders the radar blips in SVG', () => {
-    mount(<Radar history={{ push: jest.fn() }} />, { attachTo: window.domNode });
+    mount(
+      <MemoryRouter>
+        <Radar history={{ push: jest.fn() }} />
+      </MemoryRouter>,
+      { attachTo: window.domNode },
+    );
     expect(document.querySelectorAll('g.blip')).toHaveLength(2);
   });
 
   test('It is possible to click radar blips', () => {
     const clickFn = jest.fn();
-    mount(<Radar history={{ push: clickFn }} />, { attachTo: window.domNode });
+    mount(
+      <MemoryRouter>
+        <Radar history={{ push: clickFn }} />
+      </MemoryRouter>,
+      { attachTo: window.domNode },
+    );
     document.querySelectorAll('g.blip')[0].dispatchEvent(new Event('click'));
     expect(clickFn.mock.calls.length).toEqual(1);
   });
