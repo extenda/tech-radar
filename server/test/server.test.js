59dc3245c3977f59b24975acee2ba9b0acc9b5bb
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2020-03-03 20:53:36 +0100

    fix: Test server with ephemeral port

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -29,13 +29,22 @@ OAuth2Client.mockImplementation(() => ({
   verifyIdToken: mockVerifyIdToken,
 }));
 
-const server = require('../src/js/index');
-
 // Directory where static test content lives.
 // Picked up by our Express server.
 process.env.PUBLIC_HTML = path.join(__dirname, 'testdata');
 
+// Make ExpressJS use an ephemeral port.
+process.env.PORT = '0';
+
+// Now include and start the server.
+const server = require('../src/js/index');
+
 describe('Express', () => {
+  let port;
+  beforeAll(() => {
+    port = server.address().port;
+  });
+
   afterAll(() => {
     server.close();
   });
@@ -46,26 +55,26 @@ describe('Express', () => {
   });
 
   test('It responds to /', async () => {
-    const response = await fetch('http://127.0.0.1:8080/');
+    const response = await fetch(`http://127.0.0.1:${port}/`);
     expect(response.status).toEqual(200);
     expect(response.headers.get('content-type')).toEqual('text/html; charset=UTF-8');
   });
 
   test('It removes X-Powered-By-Express response header', async () => {
-    const response = await fetch('http://127.0.0.1:8080/');
+    const response = await fetch(`http://127.0.0.1:${port}/`);
     expect(response.headers.get('x-powered-by-express')).toBeNull();
   });
 
   describe('js/radar.js', () => {
     test('It returns 401 for missing token', async () => {
-      const response = await fetch('http://127.0.0.1:8080/js/radar.json');
+      const response = await fetch(`http://127.0.0.1:${port}/js/radar.json`);
       expect(response.status).toEqual(401);
       expect(response.headers.get('www-authenticate')).toContain('credentials_required');
       expect(mockVerifyIdToken).not.toHaveBeenCalled();
     });
 
     test('It returns 401 for invalid scheme', async () => {
-      const response = await fetch('http://127.0.0.1:8080/js/radar.json', {
+      const response = await fetch(`http://127.0.0.1:${port}/js/radar.json`, {
         headers: {
           authorization: 'token VALID_DOMAIN',
         },
@@ -76,7 +85,7 @@ describe('Express', () => {
     });
 
     test('It returns 401 for invalid JWT', async () => {
-      const response = await fetch('http://127.0.0.1:8080/js/radar.json', {
+      const response = await fetch(`http://127.0.0.1:${port}/js/radar.json`, {
         headers: {
           authorization: 'Bearer INVALID_TOKEN',
         },
@@ -87,7 +96,7 @@ describe('Express', () => {
     });
 
     test('It returns 401 for JWT with incorrect host domain', async () => {
-      const response = await fetch('http://127.0.0.1:8080/js/radar.json', {
+      const response = await fetch(`http://127.0.0.1:${port}/js/radar.json`, {
         headers: {
           authorization: 'Bearer INVALID_DOMAIN',
         },
@@ -98,7 +107,7 @@ describe('Express', () => {
     });
 
     test('It returns 200 OK for valid JWT', async () => {
-      const response = await fetch('http://127.0.0.1:8080/js/radar.json', {
+      const response = await fetch(`http://127.0.0.1:${port}/js/radar.json`, {
         headers: {
           authorization: 'Bearer VALID_DOMAIN',
         },
