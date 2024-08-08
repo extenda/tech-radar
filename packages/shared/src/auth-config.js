592073148eb5e349ee1de1833d08855a7f2ebce9
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2024-08-08 10:46:41 +0200

    chore: Format @tech-radar/shared with prettier

--- packages/shared/src/auth-config.js
+++ packages/shared/src/auth-config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  clientId: '947570385950-m9cgarv8pduseu61eudf3k84glo47jv0.apps.googleusercontent.com',
+  clientId:
+    '947570385950-m9cgarv8pduseu61eudf3k84glo47jv0.apps.googleusercontent.com',
   hostedDomain: 'extendaretail.com',
 };
